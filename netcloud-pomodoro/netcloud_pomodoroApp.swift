f158c1e2f139d04b9c13566f19557c2e44ea31cb
luckycod3r <belovtimofei@gmail.com>
2025-09-22 10:09:09 +0300

    Initial Commit

---
+++ netcloud-pomodoro/netcloud_pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  netcloud_pomodoroApp.swift
+//  netcloud-pomodoro
+//
+//  Created by Тимофей Белов on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct netcloud_pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
